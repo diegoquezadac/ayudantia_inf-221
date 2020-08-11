adef7f88a41050d1913fffd0e0b5a34258b800c6
Diego Quezada <57917098+diegoquezadac@users.noreply.github.com>
2020-08-11 03:57:25 -0400

    Update biseccion.py

--- scripts/biseccion.py
+++ scripts/biseccion.py
@@ -1,9 +1,10 @@
-def biseccion(f,a,b,epsilon):
-    while( (b-a)/2 > epsilon):
-        c = (a + b)/2
-        if f(c) == 0: break
-        if f(a)*f(c) < 0:
-            b = c
+def biseccion(f,a,b,epsilon): # f es una función con un cero en [a,b]
+    while((b-a)/2 > epsilon): # mientras esté cometiendo un error mayor a epsilon
+        c = (a + b)/2 # punto medio de [a,b]
+        if f(c) == 0: break 
+        if f(a)*f(c) < 0: # si el cero está en [a,c]
+            b = c # sigo trabajando en ese intervalo
         else:
-        	a = c
+        	a = c # de lo contrario está en [b,c], sigo trabajando por ahí.
+            
     return (a+b)/2
